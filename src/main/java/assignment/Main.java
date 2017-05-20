151fc5d6ba40f863726dd3d32442a4b8267f3205
constantin-p <constantin.panaitescu@yahoo.com>
2017-05-20 21:12:57 +0200

    add maven build files

--- src/main/java/assignment/Main.java
+++ src/main/java/assignment/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Parent root = FXMLLoader.load(loader.getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
